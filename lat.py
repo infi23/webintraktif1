076c5bf9bc14f0122c1b0083cdeca2cb3d771a7a
Infico <jangansampelupa200@gmail.com>
2024-02-17 16:18:58 +0900

    v3

--- lat.py
+++ lat.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 
-st.write("Coding Club 2024")
+st.write("Infinity Coding Club")
 df = pd.DataFrame({
   'No': [1, 2, 3, 4],
   'Nama' : ['Nurul','Nadia','Vino','Zahwa'],
