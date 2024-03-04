13c823cc0f305dc67bc8f2ddb6d1a1f29220e742
MuizMK <jelajahit2014@gmail.com>
2024-03-04 21:25:06 +0900

    rr2

--- index.py
+++ index.py
@@ -59,10 +59,10 @@ if __name__ == "__main__":
 
     # Add custom CSS to hide the GitHub icon
     hide_github_icon = """
-    #GithubIcon {
-    visibility: hidden;
-    }
-    """
+        <style>
+        #GithubIcon {visibility: hidden;}
+        </style>   
+        """
     st.markdown(hide_github_icon, unsafe_allow_html=True)
 
     # Your app code goes here
