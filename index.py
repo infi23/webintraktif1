bf865bbe1f29b27ec1b6f3edaeae4113a1bd18ff
MuizMK <jelajahit2014@gmail.com>
2024-03-04 20:55:55 +0900

    rr1

--- index.py
+++ index.py
@@ -46,14 +46,15 @@ def main():
                 """,
         unsafe_allow_html=True,
     )
-    hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 
 
 if __name__ == "__main__":
+    hide_streamlit_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     main()
